509b0945e225a84cb1de958c86096f34fa761df2
tarunnsingh <tarunnsingh2019@gmail.com>
2020-12-15 15:04:35 +0530

    Added Final Files

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpService } from "../http.service";
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  brews: Object;
+
+  constructor(private _http: HttpService) { }
 
   ngOnInit(): void {
+    this._http.getBreweries().subscribe(data => {
+      this.brews = data;
+      console.log(this.brews)
+    });
+
   }
 
 }
